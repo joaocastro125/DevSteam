c94fdb25f830aab785ae3fe4f2470dc5abe8a834
joaocastro125 <60033567+joaocastro125@users.noreply.github.com>
2023-07-13 06:17:34 -0300

    Update Logo.jsx

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -2,8 +2,6 @@ import Image from "next/image"
 import sytles from './logo.module.css'
 export default function logo(){
     return(
-       
-    
           <div className={sytles.logo}>
           <h1>devteam</h1>
         <Image src= '/logo.svg' width={60} height={60}/>
